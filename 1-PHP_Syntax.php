2b2e36a2a5b00bdd73e899d92a75cc3e48a0b447
‪Osama Salem‬‏ <70097854+Osama-Same@users.noreply.github.com>
2022-11-06 22:17:52 +0200

    changes

---
+++ 1-PHP_Syntax.php
@@ -0,0 +1,20 @@
+<?php
+// Basic PHP Syntax
+echo "Hello World!";
+
+
+
+//PHP Case Sensitivity
+//In PHP, keywords (e.g. if, else, while, echo, etc.), classes, functions, and user-defined functions are not case-sensitive.
+
+echo "Hello World!<br>";
+echo "Hello World!<br>";
+echo "Hello World!<br>";
+echo "Hello World!<br>";
+
+
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $color . "<br>";
+echo "My boat is " . $color . "<br>";
+?>
---
+++ 2-PHP_Data_Types.php
@@ -0,0 +1,26 @@
+
+
+<?php
+
+// PHP String
+$x = "Hello world!";
+$y = 'Hello world!';
+
+echo $x;
+echo "<br>";
+echo $y;
+echo "<br>";
+
+// PHP Integer
+
+$x = 5985;
+var_dump($x);
+echo "<br>";
+
+// PHP Float
+
+$x = 10.365;
+var_dump($x);
+echo "<br>";
+
+?>
