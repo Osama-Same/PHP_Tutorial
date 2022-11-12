945afabdbcddfc3d9a931ec24a41f8d250149e60
‪Osama Salem‬‏ <70097854+Osama-Same@users.noreply.github.com>
2022-11-12 22:04:10 +0300

    changes

--- 8-PHP_if_Statements.php
+++ 8-PHP_if_Statements.php
@@ -16,4 +16,12 @@ if ($t < "10") {
   echo "Have a good night!";
 }
 
+$year =1699;
+
+if (($year % 4 === 0 && $year % 100 !== 0) || $year % 400 === 0) {
+  echo true;
+} else {
+  echo false;
+}
+
 ?>
