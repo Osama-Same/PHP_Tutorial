a650e987cdd52d244586f27862fd9f62855bba43
‪Osama Salem‬‏ <70097854+Osama-Same@users.noreply.github.com>
2022-11-12 21:46:53 +0300

    changes

---
+++ 10-PHP_Functions.php
@@ -0,0 +1,16 @@
+<?php 
+/*
+Syntax
+function functionName() {
+  code to be executed;
+}
+*/
+
+
+//Create a User Defined Function in PHP
+function writeMsg()
+{
+  echo "Hello world!<br><br>";
+}
+writeMsg(); // call the function
+?>
